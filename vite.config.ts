97bbdce7ae4c4a40cf8c4f20a42e6f0ccf6c0059
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-01-31 00:15:11 +0600

    build: add img optimizer, svgo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin({}), ViteImageOptimizer({})],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
